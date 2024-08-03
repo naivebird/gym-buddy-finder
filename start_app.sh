9186e61635ca90024d82f24d9257e691a3224f8d
Duc Ho <hoduc1202@gmail.com>
2024-08-02 17:51:03 -0700

    Update start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+# Start backend
+
 docker-compose up -d mysql phpmyadmin
 mvn clean install
 docker-compose up -d --build app
+
+# Start frontend
 cd web
+npm install
 npm start
